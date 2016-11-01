baed11d5164560c34b55dab8e84052981728650a
John A. Fossum <john.fossum@q-free.com>
2016-11-01 09:16:49 +0100

    Adding MainHeader for imports

--- QuadFlightController/QuadFlightController.ino
+++ QuadFlightController/QuadFlightController.ino
@@ -1,12 +1,5 @@
 
-#include <JAF_EscControllerLib.h>
-#include <JAF_FrequencySchedulerLib.h>
-#include <JAF_PIDLib.h>
-#include <JAF_Razor9DOFLib.h>
-#include <JAF_UltrasonicLib.h>
-#include <JAF_WatchdogLib.h>
-
-
+#include "QuadFlightMain.h"
 
 void setup()
 {
---
+++ QuadFlightController/QuadFlightMain.h
@@ -0,0 +1,9 @@
+#include <JAF_EscControllerLib.h>
+#include <JAF_FrequencySchedulerLib.h>
+#include <JAF_PIDLib.h>
+#include <JAF_Razor9DOFLib.h>
+#include <JAF_UltrasonicLib.h>
+#include <JAF_WatchdogLib.h>
+
+#include "src\JAF_MavLink\mavlink_bridge_header.h"
+#include "src\JAF_MavLink\mavlink_receive.h"
